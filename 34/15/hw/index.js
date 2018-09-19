640083e1f9c74c84b06782d60afd25fe2bcefc75
John Doe <taranosov.k@gmail.com>
2018-09-19 11:51:01 +0300

    Add home work (15)

--- 34/15/hw/index.js
+++ 34/15/hw/index.js
@@ -4,10 +4,26 @@ const http = require('http'),
 // Используется Nodemon 1.18.4
 // Так же популярные варианты (supervisor, forever)
 
+
+var stats = fs.statSync("text.txt"),
+    fileSizeInBytes = stats.size,
+    fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
+
 http.createServer(function (req, res) {
     if(req.url === '/stream'){
-       var stream = fs.createReadStream('text.txt');
-       stream.pipe(res)
+        
+        console.log(fileSizeInMegabytes);
+
+        if (fileSizeInMegabytes > 20){
+            var stream = fs.createReadStream('text.txt');
+            stream.pipe(res);
+        }
+        else {
+            fs.readFile('text.txt', function (err, data) {
+                res.write(data);
+                res.end();
+            })
+        }
     }
     if(req.url === '/file'){
         fs.readFile('text.txt', function (err, data) {
